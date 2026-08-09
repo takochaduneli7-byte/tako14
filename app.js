1144ac5a3d8783769247894372ddca124ac835d6
tako chaduneli <takochaduneli7@gmail.com>
2026-08-10 01:35:26 +0400

    add js

---
+++ app.js
@@ -0,0 +1,10 @@
+const img = document.createElement("img");
+img.src = "https://fastly.picsum.photos/id/534/1000/800.jpg?hmac=tFbU1nZ2RnQNroI_ToBhH-LFB8KNjyWoc3bVv5G9Wkw";
+document.body.appendChild(img);
+
+const img = document.createElement("img");
+img.src = "https://fastly.picsum.photos/id/534/1000/800.jpg?hmac=tFbU1nZ2RnQNroI_ToBhH-LFB8KNjyWoc3bVv5G9Wkw";
+img.style.width = "350px";
+img.style.height = "250px";
+img.style.marginTop = "30px";
+document.body.appendChild(img);
---
+++ data.js
@@ -0,0 +1,254 @@
+export const results = [
+	{
+		id: "location-29475393",
+		type: "LOCATION",
+		content: {
+			location: {
+				id: "29475393",
+				skyCode: "IT",
+				name: "Italy",
+				type: "Nation",
+				continent: {
+					code: "EU",
+					name: "Europe",
+				},
+			},
+			flightQuotes: {
+				cheapest: {
+					price: "705 ₾",
+					rawPrice: 705,
+					direct: true,
+				},
+				direct: {
+					price: "705 ₾",
+					rawPrice: 705,
+					direct: true,
+				},
+			},
+			image: {
+				url: "https://content.skyscnr.com/f348d79cfdf70286dc759d24618a23c3/GettyImages-182281845.jpg",
+			},
+			flightRoutes: {
+				directFlightsAvailable: true,
+			},
+		},
+	},
+	{
+		id: "location-29475260",
+		type: "LOCATION",
+		content: {
+			location: {
+				id: "29475260",
+				skyCode: "PL",
+				name: "Poland",
+				type: "Nation",
+				continent: {
+					code: "EU",
+					name: "Europe",
+				},
+			},
+			flightQuotes: {
+				cheapest: {
+					price: "875 ₾",
+					rawPrice: 875,
+					direct: true,
+				},
+				direct: {
+					price: "875 ₾",
+					rawPrice: 875,
+					direct: true,
+				},
+			},
+			image: {
+				url: "https://content.skyscnr.com/16636d692d5905702ad23e3d35c65e48/GettyImages-178367416.jpg",
+			},
+			flightRoutes: {
+				directFlightsAvailable: true,
+			},
+		},
+	},
+	{
+		id: "location-29475229",
+		type: "LOCATION",
+		content: {
+			location: {
+				id: "29475229",
+				skyCode: "GR",
+				name: "Greece",
+				type: "Nation",
+				continent: {
+					code: "EU",
+					name: "Europe",
+				},
+			},
+			flightQuotes: {
+				cheapest: {
+					price: "1,040 ₾",
+					rawPrice: 1040,
+					direct: true,
+				},
+				direct: {
+					price: "1,040 ₾",
+					rawPrice: 1040,
+					direct: true,
+				},
+			},
+			image: {
+				url: "https://content.skyscnr.com/26babb582bc1d7572fed1877d7f81fa6/GettyImages-181391203.jpg",
+			},
+			flightRoutes: {
+				directFlightsAvailable: true,
+			},
+		},
+	},
+	{
+		id: "location-29475257",
+		type: "LOCATION",
+		content: {
+			location: {
+				id: "29475257",
+				skyCode: "HU",
+				name: "Hungary",
+				type: "Nation",
+				continent: {
+					code: "EU",
+					name: "Europe",
+				},
+			},
+			flightQuotes: {
+				cheapest: {
+					price: "1,055 ₾",
+					rawPrice: 1055,
+					direct: false,
+				},
+			},
+			image: {
+				url: "https://content.skyscnr.com/1326f876f67413db59cc34a466945260/GettyImages-493113190.jpg",
+			},
+			flightRoutes: {
+				directFlightsAvailable: false,
+			},
+		},
+	},
+	{
+		id: "location-29475197",
+		type: "LOCATION",
+		content: {
+			location: {
+				id: "29475197",
+				skyCode: "CY",
+				name: "Cyprus",
+				type: "Nation",
+				continent: {
+					code: "EU",
+					name: "Europe",
+				},
+			},
+			flightQuotes: {
+				cheapest: {
+					price: "1,093 ₾",
+					rawPrice: 1093,
+					direct: true,
+				},
+				direct: {
+					price: "1,093 ₾",
+					rawPrice: 1093,
+					direct: true,
+				},
+			},
+			image: {
+				url: "https://content.skyscnr.com/1e1ee1429a2277feae3d34a0cdf57886/GettyImages-135083549.jpg",
+			},
+			flightRoutes: {
+				directFlightsAvailable: true,
+			},
+		},
+	},
+	{
+		id: "location-29475389",
+		type: "LOCATION",
+		content: {
+			location: {
+				id: "29475389",
+				skyCode: "CZ",
+				name: "Czechia",
+				type: "Nation",
+				continent: {
+					code: "EU",
+					name: "Europe",
+				},
+			},
+			flightQuotes: {
+				cheapest: {
+					price: "1,169 ₾",
+					rawPrice: 1169,
+					direct: false,
+				},
+			},
+			image: {
+				url: "https://content.skyscnr.com/6f3779016c23190fadcdc6f500f5dc11/GettyImages-464420602.jpg",
+			},
+			flightRoutes: {
+				directFlightsAvailable: false,
+			},
+		},
+	},
+	{
+		id: "location-29475376",
+		type: "LOCATION",
+		content: {
+			location: {
+				id: "29475376",
+				skyCode: "CH",
+				name: "Switzerland",
+				type: "Nation",
+				continent: {
+					code: "EU",
+					name: "Europe",
+				},
+			},
+			flightQuotes: {
+				cheapest: {
+					price: "1,252 ₾",
+					rawPrice: 1252,
+					direct: false,
+				},
+			},
+			image: {
+				url: "https://content.skyscnr.com/92ec24310bc1e0c2c5022132dd1d4d55/GettyImages-178766147.jpg",
+			},
+			flightRoutes: {
+				directFlightsAvailable: false,
+			},
+		},
+	},
+	{
+		id: "location-29475385",
+		type: "LOCATION",
+		content: {
+			location: {
+				id: "29475385",
+				skyCode: "FR",
+				name: "France",
+				type: "Nation",
+				continent: {
+					code: "EU",
+					name: "Europe",
+				},
+			},
+			flightQuotes: {
+				cheapest: {
+					price: "1,268 ₾",
+					rawPrice: 1268,
+					direct: false,
+				},
+			},
+			image: {
+				url: "https://content.skyscnr.com/c88da2b091534f7baf8536b3959ce83a/GettyImages-495057957.jpg",
+			},
+			flightRoutes: {
+				directFlightsAvailable: true,
+			},
+		},
+	},
+];
