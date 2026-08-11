3109e6c206dd15a8d0b58a76a0df66a2be8802a6
tako chaduneli <takochaduneli7@gmail.com>
2026-08-11 19:41:51 +0400

    some

--- app.js
+++ app.js
@@ -1,10 +1,49 @@
-const img = document.createElement("img");
-img.src = "https://fastly.picsum.photos/id/534/1000/800.jpg?hmac=tFbU1nZ2RnQNroI_ToBhH-LFB8KNjyWoc3bVv5G9Wkw";
-document.body.appendChild(img);
+import { results } from "./data.js";
 
-const img = document.createElement("img");
-img.src = "https://fastly.picsum.photos/id/534/1000/800.jpg?hmac=tFbU1nZ2RnQNroI_ToBhH-LFB8KNjyWoc3bVv5G9Wkw";
-img.style.width = "350px";
-img.style.height = "250px";
-img.style.marginTop = "30px";
-document.body.appendChild(img);
+const flightList = document.querySelector("#flight-list");
+
+flightList.innerHTML = results
+  .map((item) => {
+    const location = item.content.location;
+    const flight = item.content.flightQuotes.cheapest;
+    const image = item.content.image.url;
+
+    return `
+      <div class="flight-card">
+
+        <img
+          class="flight-image"
+          src="${image}"
+          alt="${location.name}"
+        >
+
+        <div class="flight-info">
+
+          <div class="flight-header">
+            <h2>${location.name}</h2>
+
+            <span class="flight-price">
+              ${flight.direct ? "Direct" : "With stop"}, ${flight.price} €
+            </span>
+          </div>
+
+          <div class="flight-buttons">
+            <button class="details-button">
+              See more details
+            </button>
+
+            <button class="remove-button">
+              Remove Flight
+            </button>
+          </div>
+
+        </div>
+
+      </div>
+    `;
+  })
+  .join("");
+
+
+
+  
